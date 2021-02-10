f2ebe950965e201ae9890fc7a9b31870f81b8a3c
Dung Ho <mclanren156@gmail.com>
2021-02-10 09:57:34 +0700

    33. Working on the Post Page

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="posts-list">
+    <PostPreview
+      id="1"
+      thumbnail="/Shutter.Island.jpg"
+      title="Hello there!"
+      previewText="This is my first post!"
+    />
+    <PostPreview
+      id="2"
+      thumbnail="/Shutter.Island.jpg"
+      title="Hello there - the second time!"
+      previewText="This is my second post!"
+    />
+    <PostPreview
+      id="3"
+      thumbnail="/Shutter.Island.jpg"
+      title="Hello there - the third time!"
+      previewText="This is my third post!"
+    />
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import PostPreview from "@/components/Posts/PostPreview.vue";
+
+export default Vue.extend({
+  components: {
+    PostPreview,
+  },
+});
+</script>
+
+<style scoped>
+.posts-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ pages/posts/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="posts-page">
+    <PostList />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import PostList from "@/components/Posts/PostList.vue";
+
+export default Vue.extend({
+  components: {
+    PostList,
+  },
+});
+</script>
+
+<style scoped>
+.post-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
